7cc29c18de13cf3f9a80c24ba8c3e406c164d2e0
olawhales <ajaditaoreed@gmail.com>
2025-10-02 23:36:16 +0100

    updates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -7,37 +7,28 @@ import WhyChooseUs from "../components/WhyChooseUs";
 import CTA from "../components/CTA";
 import Footer from "../components/Footer";
 
-// const Index = () => {
-//   return (
-//     <div className="">
-//       <Navbar />
-//       <div className="max-w-9xl mx-auto px-7">
-//         {/* About Section */}
-//         <section id="about">
-//           <About />
-//         </section>
-
-//         {/* Services Section */}
-//         <section id="services">
-//           <Services />
-//         </section>
-//         <section id="why-choose-us">
-//           <WhyChooseUs />
-//         </section>
-//         <CTA />
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// };
 const Index = () => {
   return (
-    <div>
-      {/* <Navbar /> */}
-      <h1>Testing Index Page</h1>
+    <div className="">
+      <Navbar />
+      <div className="max-w-9xl mx-auto px-7">
+        {/* About Section */}
+        <section id="about">
+          <About />
+        </section>
+
+        {/* Services Section */}
+        <section id="services">
+          <Services />
+        </section>
+        <section id="why-choose-us">
+          <WhyChooseUs />
+        </section>
+        <CTA />
+        <Footer />
+      </div>
     </div>
   );
 };
-
 
 export default Index;
